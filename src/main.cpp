7d648d540d0fdc92bbfd7eb99c89513030d0451a
Denis Schneider <denis.schneider@nerdtronix.de>
2020-06-03 17:59:29 +0200

    Added Async Wifimanager, Clear creds may be triggered by button

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,33 @@
 #include <ESPAsyncWebServer.h>
 #include "SPIFFS.h"
 
-
-const char* ssid = "WLAN HOUSE";
-const char* password =  "wioed9dxjsndcio38i3ndcklw";
+//needed for library
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h>
+#include "esp_wifi.h"
 
 AsyncWebServer server(80);
-
-
+DNSServer dns;
 
 void setup()
 {
 	//inti serial console
 	Serial.begin(115200);
 
+	//WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    AsyncWiFiManager wifiManager(&server,&dns);
+    
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("pumpe_ard");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+	
+	//if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
 
 	//mounting filesystem
 	if(!SPIFFS.begin())
@@ -25,20 +39,6 @@ void setup()
         return;
   	}
  
-
-	//starting wifi and attempt do connect to network
-	WiFi.begin(ssid, password);
-
-	//wait for connection esablished
-	while (WiFi.status() != WL_CONNECTED)
-	{
-  		delay(500);
-  		Serial.println("Connecting to WiFi..");
-	}
-
-	//connection established
-	Serial.println("Connected to the WiFi network");
-
 	//starting mDNS service
 	if(!MDNS.begin("pumpe"))
 	{
@@ -53,17 +53,33 @@ void setup()
   	server.on("/html", HTTP_GET, [](AsyncWebServerRequest *request)
 	{
     	request->send(SPIFFS, "/index.html", "text/html");
+		Serial.println("Sending requested index.html");
 	});
   
 	//start server
   	server.begin();
 }
 
-
-
 void loop()
 {
 	Serial.println("idle");
-
 	delay(5000);
+}
+
+void clear_wifi_credentials()
+{
+	//reset saved settings
+	//wifiManager.resetSettings();
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); //load the flash-saved configs
+	esp_wifi_init(&cfg); //initiate and allocate wifi resources (does not matter if connection fails)
+	delay(2000); //wait a bit
+	if(esp_wifi_restore()!=ESP_OK)
+	{
+		Serial.println("WiFi is not initialized by esp_wifi_init ");
+		}else{
+			Serial.println("WiFi Configurations Cleared!");
+		}
+		//continue
+	delay(1000);
+	esp_restart(); //just my reset configs routine...
 }
