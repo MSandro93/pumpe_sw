06f88428b3225da7a704d4dddd8a546f74e5de6c
Michael <->
2020-06-06 02:25:55 +0200

    ... and now server crashes if client-browser requests server time. Working on this one...

--- src/main.cpp
+++ src/main.cpp
@@ -82,7 +82,7 @@ void handleRequest(AsyncWebServerRequest *request, uint8_t *data, size_t len, si
 
   		sprintf(buff, " %02d:%02d:%02d  %02d.%02d.%04d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec, timeinfo.tm_mday, timeinfo.tm_mon, (timeinfo.tm_year + 1900));
 
-		request->send(200, "text/plain", buff);
+		request->send(200, "text/plain", buff); 
 	}
 
 	else
